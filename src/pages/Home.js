e66699a583d6d58447eed871f03bde54c7e99b8c
kbeloin <kyle.beloin@me.com>
2022-03-11 21:23:53 -0700

    Updates to homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,20 @@ export const Home = () => {
   const title = "Home";
   const content = (
     <p>
-      Thank you for visiting my website! If you would like to see my recent work, please check out my <a className="ui" href={"https://www.mariakostromitina.com/publications"}>
-            Publications </a> page. You can also find my past and current research projects on the <a className="ui" href={"https://www.mariakostromitina.com/projects"}> Projects </a> page. You can visit the <a className="ui" href={"https://www.mariakostromitina.com/cv"}> CV </a> page to learn about my teaching experience as well as service and awards. 
-          </a>
+      Thank you for visiting my website! If you would like to see my recent
+      work, please check out my{" "}
+      <a className="ui" href={"/publications"}>
+        Publications
+      </a>{" "}
+      page. You can also find my past and current research projects on the{" "}
+      <a className="ui" href={"/projects"}>
+        Projects
+      </a>{" "}
+      page. You can visit the{" "}
+      <a className="ui" href={"/cv"}>
+        CV
+      </a>{" "}
+      page to learn about my teaching experience as well as service and awards.
     </p>
   );
 
