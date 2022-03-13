542b7a2d5e0ae451195d8312fb517b042e305633
kbeloin <kyle.beloin@me.com>
2022-03-13 13:32:49 -0700

    Service Worker Config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./index.css";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+
+serviceWorkerRegistration.register();
 
 const rootElement = document.getElementById("root");
 render(
