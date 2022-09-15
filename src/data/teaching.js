1c2287f9bdbaca12a9bddfd5d7e95d2b6ec58abc
kbeloin <kyle.beloin@me.com>
2022-09-14 23:57:30 -0700

    Added teaching data (university).

---
+++ src/data/teaching.js
@@ -0,0 +1,139 @@
+export const teaching = [
+  {
+    level: "University",
+    institution: "Northern Arizona University",
+    courses: [
+      // ENG 559: ESL Methods and Materials: Reading and Writing
+      // Summer 2020 (online)
+      {
+        number: "ENG 559",
+        title: "ESL Methods and Materials: Reading and Writing",
+        role: "Instructor",
+        sections: [
+          {
+            year: "2020",
+            semester: "Summer",
+            location: "online",
+          },
+        ],
+      },
+      // ENG 538: Cross-Cultural Aspects of Language Learning
+      {
+        number: "ENG 538",
+        title: "Cross-Cultural Aspects of Language Learning",
+        role: "Instructor",
+        sections: [
+          {
+            year: "2022",
+            semester: "Summer",
+            location: "online",
+          },
+        ],
+      },
+      {
+        // BME 480: Parent and Community Involvement in ESL and Bilingual Programs Winter 2021 (online)
+        number: "BME 480",
+        title: "Parent and Community Involvement in ESL and Bilingual Programs",
+        role: "Instructor",
+        sections: [
+          {
+            year: "2021",
+            semester: "Winter",
+            location: "online",
+          },
+        ],
+      },
+      {
+        number: "ENG 205",
+        title: "The Academic Writer's Workshop",
+        // Fall 2017; Spring 2018; Summer 2018 (hybrid)
+        role: "Instructor",
+        sections: [
+          {
+            year: "2018",
+            semester: "Summer",
+            location: "hybrid",
+          },
+          {
+            year: "2018",
+            semester: "Spring",
+          },
+          {
+            year: "2017",
+            semester: "Fall",
+          },
+        ],
+      },
+      {
+        number: "ENG 105",
+        title: "Critical Reading and Writing in the University Community",
+        role: "Instructor",
+        sections: [
+          {
+            year: "2021",
+            semester: "Spring",
+          },
+          {
+            year: "2020",
+            semester: "Fall",
+          },
+          {
+            year: "2020",
+            semester: "Spring",
+          },
+          {
+            year: "2019",
+            semester: "Fall",
+          },
+          {
+            year: "2019",
+            semester: "Spring",
+          },
+          {
+            year: "2018",
+            semester: "Fall",
+          },
+          {
+            year: "2017",
+            semester: "Summer",
+          },
+          {
+            year: "2017",
+            semester: "Spring",
+          },
+          {
+            year: "2016",
+            semester: "Fall",
+          },
+        ],
+      },
+
+      {
+        // ENG 528: Grammatical Foundations
+        // Spring 2022
+        number: "ENG 528",
+        title: "Grammatical Foundations",
+        role: "Teaching Assistant",
+        sections: [
+          {
+            year: "2022",
+            semester: "Spring",
+          },
+        ],
+      },
+      //  ENG 308: Introduction to Linguistics
+      // Fall 2022
+      {
+        number: "ENG 308",
+        title: "Introduction to Linguistics",
+        role: "Teaching Assistant",
+        sections: [
+          {
+            year: "2022",
+            semester: "Fall",
+          },
+        ],
+      },
+    ],
+  },
+];
