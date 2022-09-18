7eba057d849a1c9b1a8256b965de044d9260e5e9
kbeloin <kyle.beloin@me.com>
2022-09-18 13:23:09 -0700

    service

--- src/data/service.js
+++ src/data/service.js
@@ -1,5 +1,3 @@
-import { Header } from "semantic-ui-react";
-
 export const service = [
   {
     year: "2021-present",
@@ -26,40 +24,5 @@ export const service = [
     institution: "Northern Arizona University",
   },
 ];
-
-export const serviceElement = (service, i) => {
-  let el;
-
-  el = (
-    <Header key={`service-container-${i}`} size="small">
-      <Header.Subheader style={{ marginBottom: "0" }}>
-        <p style={{ marginBottom: "0" }}>
-          {service.organization}{" "}
-          <span className="year--emphasis">({service.year})</span>
-        </p>
-      </Header.Subheader>
-      <Header
-        as="a"
-        key={`service-title-${i}`}
-        size="tiny"
-        className="disabled__link"
-      >
-        {service.role}
-      </Header>
-      <Header.Subheader>
-        <p
-          style={{
-            marginBottom: "0",
-            fontWeight: "100",
-            opacity: 0.6,
-          }}
-        >
-          {service.institution}
-        </p>
-      </Header.Subheader>
-    </Header>
-  );
-  return el;
-};
 
 export default service;
